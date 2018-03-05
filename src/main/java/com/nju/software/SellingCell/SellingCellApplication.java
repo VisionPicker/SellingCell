5a3cd139ef3f540e1ddbe17134cbb0dee5e9e637
keroro <keroro_exe@163.com>
2018-03-05 12:03:55 +0800

    project init

---
+++ src/main/java/com/nju/software/SellingCell/SellingCellApplication.java
@@ -0,0 +1,12 @@
+package com.nju.software.SellingCell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingCellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingCellApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nju/software/SellingCell/controller/AuthorizationController.java
@@ -0,0 +1,8 @@
+package com.nju.software.SellingCell.controller;
+
+/**
+ * Created by keroro on 2018/2/24.
+ */
+public class AuthorizationController
+{
+}
